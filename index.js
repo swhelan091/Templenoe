8c674e5acff201c4f849aa569383152fc828c476
Steve Whelan <swhelan091@gmail.com>
2017-04-15 21:48:58 -0400

    delete unneeded file

--- index.js
+++
@@ -1,9 +0,0 @@
-const betsController = require('../controllers').bets;
-
-module.exports = (app) => {
-  app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to Templenoe!',
-  }));
-
-  app.post('/api/bets', betsController.create);
-};
